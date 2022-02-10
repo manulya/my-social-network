a617af2227f4514f3cec8e0edd69e15771969437
Manuilova-Ulyana <ylianadans@gmail.com>
2022-02-10 10:05:53 +0300

    34lessons

--- src/components/News/News.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import s from './News.module.css';
-
-const News = (props) =>{
-    return(
-<div>News</div>
-    );
-};
-export default News;
--- src/components/Settings/Settings.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import s from "./Settings.module.css";
-
-const Settings = (props) => {
-  return <div>Settings</div>;
-};
-export default Settings;
