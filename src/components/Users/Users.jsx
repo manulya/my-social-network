4689453e1d335d372ca31ac47f3a53f6ad676c5e
Manuilova-Ulyana <ylianadans@gmail.com>
2022-12-05 21:52:33 +0300

    lesson 54 (componentDidMount)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,13 @@ import axios from "axios";
 import def_ava from '../../img/avatar.png';
 
 class Users extends React.Component{
-  constructor(props){
-  super(props)
-  axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+    
+  componentDidMount(){
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
     this.props.setUsers(response.data.items)
-  })
-
+  }
+    )
 }
-  
-  
 
   render(){
   return <div >
